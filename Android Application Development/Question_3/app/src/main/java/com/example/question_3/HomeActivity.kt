8e2ac80ca1e872e73db9958c34cc21ed08d09f9a
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2023-03-16 15:18:31 +0530

    Add android question_3

---
+++ Android Application Development/Question_3/app/src/main/java/com/example/question_3/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.question_3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
